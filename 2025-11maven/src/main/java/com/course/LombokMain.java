d0bcc2bed2628662eef4d52509b4c1d08d7af417
DESKTOP-P4UUKAO\student <peterli931119@gmail.com>
2025-11-22 16:35:57 +0800

    2025_11_22

---
+++ 2025-11maven/src/main/java/com/course/LombokMain.java
@@ -0,0 +1,13 @@
+package com.course;
+
+import com.course.model.User;
+
+public class LombokMain {
+
+	public static void main(String[] args) {
+		User user = new User();
+		//user.setName("Hello Kitty");
+
+	}
+
+}
